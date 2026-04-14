a01bc802386634ea35435eb206468b07c7599a48
RetanaAlvarez <chepe_jm10@hotmail.com>
2026-04-13 20:45:54 -0600

    add data js hero y projects

---
+++ src/data/homeData.js
@@ -0,0 +1,32 @@
+export const homeData = {
+    navbar: {
+        logo: "WebDev Pro",
+        cta: "Hablar por WhatsApp",
+        whatsappNumber: "527121933662",
+        message: "Hola, Me podria compartir mas informacion por favor."
+    },
+    hero: {
+        badge: "⚡Solo 3 espacios disponibles este mes",
+        title: {
+            normal: "Creo páginas web que ayudan a ",
+            highlight: "crecer tu negocio",
+        },
+        description:
+            "Sitios web rápidos, modernos y accesibles para pequeños negocios en México",
+        bullets: [
+            "✅ No necesitas conocimientos técnicos",
+            "✅ Respuesta en menos de 24 horas",
+            "✅ Inicio inmediato de tu proyecto",
+        ],
+        botones: [
+            {
+                nombre: "Solicitar mi página",
+                ruta: "#contacto"
+            },
+            {
+                nombre: "Ver Proyecto",
+                ruta: "#proyecto"
+            }
+        ]
+    }
+};
