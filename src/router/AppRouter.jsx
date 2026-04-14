5252059066d3e60b6595bc5abf0fd14c0344b6ce
RetanaAlvarez <chepe_jm10@hotmail.com>
2026-04-13 20:43:51 -0600

    add router

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "../components/layout/Navbar/Navbar";
+
+import Home from "../pages/Home/Home";
+import Projects from "../pages/Projects/Projects";
+import DemoProjects from "../pages/DemoProjects/DemoProjects";
+import RealProjects from "../pages/RealProjects/RealProjects";
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/demos" element={<DemoProjects />} />
+        <Route path="/real-projects" element={<RealProjects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
