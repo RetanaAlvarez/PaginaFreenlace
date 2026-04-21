71babed80baa221b4074ddeccbe80f10b8c4fc30
RetanaAlvarez <chepe_jm10@hotmail.com>
2026-04-21 16:34:54 -0600

    add configuracion page proyects

---
+++ src/data/projectsData.js
@@ -0,0 +1,80 @@
+
+import restauranteImg from "../assets/restaurante.png";
+import tiendaImg from "../assets/tienda.png";
+import consultorioImg from "../assets/consultorio.png";
+import empresaImg from "../assets/empresa.png";
+
+export const projectsData = {
+    project: {
+        titulo: "Proyectos",
+        descripcion: "Tu sitio web puede verse así o mejor, adaptado a tu negocio.",
+        demo: [{
+            icono: restauranteImg,
+            encabezado: "Restaurante",
+            descripcion: "Muestra tu menú y recibe pedidos fácilmente por WhatsApp",
+            lista: [
+                "Menú digital interactivo",
+                "Botón directo a WhatsApp",
+                "Galería de platillos"
+            ],
+            ruta: "/Restaurante",
+
+            color: {
+                bg: "linear-gradient(to bottom, #f8802a, #fbbcbc)",
+                border: "#fff",
+                button: "#ef4444"
+            }
+
+        }, {
+            icono: tiendaImg,
+            encabezado: "Tienda en Línea",
+            descripcion: "Muestra tus productos y recibe pedidos por WhatsApp",
+            lista: [
+                "Catálogo de productos",
+                "Carrito de compras",
+                "Filtros y búsqueda"
+            ],
+            ruta: "/Tienda",
+
+            color: {
+                bg: "linear-gradient(to bottom, #5898ff, #b3f2fc)",
+                border: "#fff",
+                button: "#3b82f6"
+            }
+        }, {
+            icono: consultorioImg,
+            encabezado: "Consultorio",
+            descripcion: "Facilita citas y mejora la atención a tus pacientes",
+            lista: [
+                "Sistema de citas Online",
+                "Información de servicios",
+                "Contacto directo"
+            ],
+            ruta: "/Consultorio",
+
+            color: {
+                bg: "linear-gradient(to bottom, #9244db, #ff80bf)",
+                border: "#fff",
+                button: "#a855f7"
+            }
+        }, {
+            icono: empresaImg,
+            encabezado: "Empresa",
+            descripcion: "Presenta tus servicios de forma Profesional",
+            lista: [
+                "Portafolio de servicios",
+                "Casos de éxito",
+                "Formulario de contacto"
+            ],
+            ruta: "/Empresa",
+
+            color: {
+                bg: "linear-gradient(to bottom, #2ff879, #90f5d3)",
+                border: "#fff",
+                button: "#16a34a"
+            }
+        }],
+        boton: "Ver ejemplo",
+        comentario: "Algunos ejemplos son demostrativos y pueden adaptarse completamente a tu negocio."
+    }
+};
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { projectsData } from "../../data/projectsData";
+
+import styles from "./Projects.module.css";
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+    const { titulo, descripcion, demo, boton, comentario } = projectsData.project;
+
+    return (
+        <section className={styles.container}>
+
+            <h1 className={styles.titulo}>{titulo}</h1>
+            <p className={styles.descripcion}>{descripcion}</p>
+
+            <div className={styles.card}>
+                {demo.map((item, index) => (
+                    <div key={index} className={styles.intocard} style={{ background: item.color.bg }}>
+                        
+                        {/* HEADER */}
+                        <div
+                            className={styles.header}
+                            style={{ backgroundImage: `url(${item.icono})` }}
+                        >
+
+                            <img src={item.icono} alt={item.encabezado} />
+                        </div>
+
+                        {/* BODY */}
+                        < div className={styles.body} >
+                            <h3>{item.encabezado}</h3>
+                            <p>{item.descripcion}</p>
+
+                            <ul>
+                                {item.lista.map((texto, key) => (
+                                    <li key={key}>✔ {texto}</li>
+                                ))}
+                            </ul>
+
+                            <a
+                                href={item.ruta}
+                                className={styles.button}
+                                style={{
+                                    border: `2px solid ${item.color.button}`,
+                                    color: item.color.border,
+                                    background: item.color.button
+                                }}
+                            >
+                                {boton}
+                            </a>
+                        </div>
+
+                    </div>
+                ))
+                }
+            </div >
+
+            <p className={styles.comentario}>{comentario}</p>
+
+        </section >
+    );
 }
 
-export default Projects
+export default Projects;
